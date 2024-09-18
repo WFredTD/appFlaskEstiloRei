7cd55d3a165eb09ed06215504397c0087161014f
Walter F. T. Dreyer <fredtorresdreyer@gmail.com>
2024-09-17 21:39:32 -0300

    Prova IV completa com checkbox e JS

--- start.py
+++ start.py
@@ -1,18 +1,35 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, session, url_for, flash
+from usuario import Usuario
 app = Flask (__name__)
+
+app.secret_key = 'supersecretkey'
+
+usuario1 = Usuario('rafael.gimenez@gmail.com', 'JaRafael', 'JavaScript')
+usuario2 = Usuario('romulo.silvestre@gmail.com', 'SilverRon', 'Sistema_S')
+usuario3 = Usuario('suzane.hospital@gmail.com', 'MarZu', 'Santa_SUS')
+
+usuarios = { usuario1.email : usuario1,
+            usuario2.email: usuario2,
+            usuario3.email : usuario3 }
 
 @app.route('/')
 def iniciar():
-    return 'inicio'
+    return render_template ('index.html')
 
-@app.route('/login')
-def login():
-    return 'login'
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    if request.form['email'] in usuarios:
+        usuario = usuarios [request.form['email']]
+        if request.form['senha'] == usuario.password:
+            session['nickname_logado'] = usuario.nickname
+            return redirect('agenda')
+        else:
+            return redirect ('/')
 
 @app.route('/logout')
 def logout():
-    return 'logout'
+    session.pop('usuario_logado', None)
+    return redirect (url_for('iniciar'))
 
 @app.route('/cadastrarBarbeiro')
 def cadastrarBarbeiro():
@@ -20,7 +37,7 @@ def cadastrarBarbeiro():
 
 @app.route('/cadastrarCliente')
 def cadastrarCliente():
-    return 'cadastrar cliente'
+    return render_template ('cadastrarCliente.html')
 
 @app.route('/cadastrarAgendamento')
 def cadastrarAgendamento():
@@ -38,9 +55,13 @@ def listaBarbeiros():
 def listaClientes():
     return 'lista de clientes'
 
-@app.route('/agenda')
+@app.route('/agenda', methods=['GET', 'POST'])
 def agenda():
-    return 'agenda'
+    if 'nickname_logado' in session:
+        nickname_logado = session.get('nickname_logado')
+        return render_template ('agenda.html', nickname_logado = nickname_logado)
+    else:
+        return redirect ('index')
 
 @app.route('/listaServico')
 def listaServico():
---
+++ usuario.py
@@ -0,0 +1,5 @@
+class Usuario():
+    def __init__(self, email, nickname, password):
+        self.email = email
+        self.nickname = nickname
+        self.password = password
